fab81cb5c66b1dd8cdb463ce6b2d09ad045ad727
Visitorx7 <116057247+harsSharma45@users.noreply.github.com>
2024-12-08 13:54:46 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int main()
+{
+  unordered_set<string> st = {"echo", "exit", "type"};
 
-int main() {
 
-  while(true){
-  cout << unitbuf;
-  cerr << unitbuf;
+  while (true)
+  {
+    cout << unitbuf;
+    cerr << unitbuf;
+    cout << "$ ";
 
-  
-  cout << "$ ";
+    string input;
+    getline(cin, input);
 
-  string input;
-  getline(cin, input);  
-  stringstream ss(input);
-  string a,b;
-  ss>>a>>b;
-  if(a=="exit"){
-    int tmp = 0;
-    if(!b.empty()){
-      try{
-        tmp = stoi(b);
+    if (cin.eof())
+    {
+      break;
+    }
+
+    stringstream ss(input);
+    string command;
+    ss >> command;
+    if (command == "exit")
+    {
+      int extCode = 0;
+      string arg;
+      ss >> arg;
+      if (!arg.empty())
+      {
+        try
+        {
+          extCode = stoi(arg);
+        }
+        catch (const exception &e)
+        {
+          cerr << "Invalid exit code" << arg << endl;
+          continue;
+        }
       }
-      catch(const exception& e){
-        cerr<<"Invalid exit code"<<b<<endl;
-        continue;
+      return extCode;
+    }
+    else if (command == "echo")
+    {
+      string message;
+      getline(ss, message);
+
+      if (!message.empty() && message[0] == ' ')
+      {
+        message = message.substr(1);
+      }
+      cout << message << endl;
+    }
+    else if(command=="type"){
+      string str;
+      ss>>str;
+      if(st.find(str)!=st.end()){
+        cout<<str<<" is a shell builtin"<<endl;
+      }
+      else if(!str.empty()){
+        cout<<str<<": not found"<<endl;
       }
     }
-    return tmp;
+    else if (!command.empty())
+    {
+      cout << input << ": command not found" << endl;
+    }
   }
-    cout<<input<<": command not found"<<endl;
-  }
-  
-
 }
